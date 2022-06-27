acd0ee552e9eaa8cf83f2067aa5074a057c614c7
Farhan Elias <faarhaan10@gmail.com>
2022-06-28 03:34:03 +0600

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Pages/Home/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './Pages/Authentication/Login';
+import Register from './Pages/Authentication/Register';
+import Navigation from './Pages/Shared/Navigation';
+import AllProducts from './Pages/Products/AllProducts';
+import ProductDetails from './Pages/Products/ProductDetails/ProductDetails';
+import RequireAuth from './RequireAuth/RequireAuth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<AllProducts />} />
+        <Route path="/product/:id" element={
+          <RequireAuth>
+            <ProductDetails />
+          </RequireAuth>
+        } />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
